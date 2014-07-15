70b92bb84a76e50dd14b8dbb4d724073635a23e0
Cleydson Júnior <cleydson.fibonacci@solucoesageis.com.br>
2014-07-15 17:06:55 -0300

    upgrading grails version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,6 +9,12 @@ grails.project.dependency.resolution = {
 		grailsCentral()
 	}
 
+	dependencies {
+		test('org.spockframework:spock-grails-support:0.7-groovy-2.0') {
+			excludes 'groovy', 'groovy-all'
+		}
+	}
+
 	plugins {
 		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
@@ -24,6 +30,7 @@ grails.project.dependency.resolution = {
 
 		test ':spock:0.7', {
 			export = false
+			exclude 'spock-grails-support'
 		}
 	}
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -10,3 +10,5 @@ grails.doc.authors="Kim A. Betti"
 grails.doc.license="Apache 2.0"
 grails.doc.copyright=""
 grails.doc.footer="Have a nice day!"
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
